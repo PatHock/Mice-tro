6bd8649861e3448d85755312e1862420d5e921e3
rbosseIII <rbosse@ycp.edu>
2018-03-15 16:21:08 -0400

    added additional functions

--- src/main/java/com/micetr0/model/Account.java
+++ src/main/java/com/micetr0/model/Account.java
@@ -1,4 +1,58 @@
 package com.micetr0.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Account {
+
+    public Account(){
+        // empty constructor
+    }
+
+    String password = null;
+    String username = null;
+    List<Composition> compositions = new ArrayList<Composition>();
+
+
+    public String getPassword() {
+        return password;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public boolean checkCredentials(String username, String Password){
+        throw new UnsupportedOperationException();
+    }
+
+    public Account createAccount(String username, String Password){
+        throw new UnsupportedOperationException();
+    }
+
+    public void deleteAccount(Account account)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    public void addComposition()
+    {
+        throw new UnsupportedOperationException();
+    }
+    public void removeComposition()
+    {
+        throw new UnsupportedOperationException();
+    }
+    public void logOut(Account account){
+        throw new UnsupportedOperationException();
+    }
+    private Account logIn(String username, String password){
+        throw new UnsupportedOperationException();
+    }
+
 }
