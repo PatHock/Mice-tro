184454336fedd446e15def3bc25bbf2ac2d20bab
aredhouse <aredhous@ycp.edu>
2018-04-05 15:49:48 -0400

    test commit :)

--- src/test/java/com/micetr0/controller/MeasureControllerTest.java
+++ src/test/java/com/micetr0/controller/MeasureControllerTest.java
@@ -18,8 +18,30 @@ public class MeasureControllerTest {
     @Before
     public void setUp()
     {
+        model = new Measure();
         controller = new MeasureController();
+        controller.setModel(model);
     }
 
+    @Test
+    public void createMeasureTest(){
+        Measure testMeasure = controller.createMeasure(50,"1","2");
+        assertTrue(testMeasure.getMaxNumBeats().equals(50));
+        assertTrue(testMeasure.getMeasureID().equals("1"));
+        assertTrue(testMeasure.getSectionID().equals("2"));
+    }
+
+    @Test
+    public void deleteMeasureTest(){
+        //fix after implementing delete measure
+        boolean result;
+        result = controller.deleteMeasure();
+        assertFalse(result);
+    }
+
+    @Test
+    public void addMeasureTest(){
+        //add after implementing add measure
+    }
 
 }
--- src/test/java/com/micetr0/model/MeasureTest.java
+++ src/test/java/com/micetr0/model/MeasureTest.java
@@ -8,4 +8,41 @@ import static org.junit.Assert.*;
 public class MeasureTest {
 //    private Note note = new Note();
     private Measure measure = new Measure();
+
+    @Test
+    public void getMaxNumBeatsTest(){
+        measure.setMaxNumBeats(50);
+        assertTrue(measure.getMaxNumBeats().equals(50));
+    }
+
+    @Test
+    public void setMaxNumBeatsTest(){
+        measure.setMaxNumBeats(10);
+        assertTrue(measure.getMaxNumBeats().equals(10));
+    }
+
+    @Test
+    public void getMeasureIDTest(){
+        measure.setMeasureID("12");
+        assertEquals(measure.getMeasureID(),"12");
+    }
+
+    @Test
+    public void setMeasureIDTest(){
+        measure.setMeasureID("15");
+        assertEquals(measure.getMeasureID(),"15");
+    }
+
+    @Test
+    public void getSectionIDTest(){
+        measure.setSectionID("3");
+        assertEquals(measure.getSectionID(),"3");
+    }
+
+    @Test
+    public void setSectionIDTest(){
+        measure.setSectionID("5");
+        assertEquals(measure.getSectionID(),"5");
+    }
+
 }
