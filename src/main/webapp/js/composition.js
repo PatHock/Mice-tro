4bb702c23529f567d84dfe96dbee03b992cdee69
PatHock <phock@ycp.edu>
2018-05-05 22:41:25 -0400

    Make composition responsive and resizable. Experiment with abcjs

--- src/main/webapp/js/composition.js
+++ src/main/webapp/js/composition.js
@@ -6,6 +6,7 @@ window.$ = require('jquery');
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../scss/style.css';
 import '../scss/composition.css';
+// https://configurator.abcjs.net/visual
 
 let abc = "T: Cooley's\n" +
     "M: 4/4\n" +
@@ -15,12 +16,33 @@ let abc = "T: Cooley's\n" +
     "|:D2|EB{c}BA B2 EB|~B2 AB dBAG|FDAD BDAD|FDAD dAFD|\n" +
     "EBBA B2 EB|B2 AB defg|afe^c dBAF|DEFD E2:|\n" +
     "|:gf|eB B2 efge|eB B2 gedB|A2 FA DAFA|A2 FA defg|\n" +
-    "eB B2 eBgB|eB B2 defg|afe^c dBAF|DEFD E2:|";
+    "eB B2 eBgB|eB B2 defg|afe^c|    |";
+// "eB B2 eBgB|eB B2 defg|afe^c dBAF|DEFD E2:|";
+
 
 function load() {
-    abcjs.renderAbc("compAbcjs", abc);
+    return abcjs.renderAbc(
+        "compAbcjs",
+        abc,
+        {
+            add_classes: true,
+            clickListener: function alertOnClick(abcElem) {
+                alert("Type " + abcElem.el_type+ "\n" +
+                "Pitch: " + abcElem.pitches[0].pitch + "\n" +
+                "Duration: " + abcElem.duration + "\n" +
+                    "Note Index: " + abcElem.measureNum
+            );
+                console.log(abcElem);
+            },
+            responsive: "resize",
+        });
 }
 
+// function alertOnClick(abcElem) {
+//     alert("Type " + abcElem.el_type);
+//     console.log(abcElem);
+// }
+
 $('document').ready(function(){
-    load();
+    let tuneObjectArray = load();
 });
